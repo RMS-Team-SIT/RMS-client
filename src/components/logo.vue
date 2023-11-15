a9136dc49896a0f215270b3a71b3c79281b326f1
siraom15 <siriwatjaiyungyuen@gmail.com>
2023-11-15 12:39:38 +0700

    First Init

---
+++ src/components/logo.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+  icon: {
+    type: Object,
+    default: null,
+  },
+  iconClass: {
+    type: String,
+    default: '',
+  },
+  text: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+<template>
+  <button
+    class="inline-flex items-center space-x-1 rounded-md bg-coral-red-hover py-3 px-4 font-medium text-indigo-50 shadow-xl transition duration-300 hover:bg-coral-red hover:shadow-md"
+  >
+    <span v-if="icon" class="w-5 h-5">
+      <component :is="icon" :class="iconClass" />
+    </span>
+    <span>{{ text }}</span>
+  </button>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { onMounted } from 'vue';
+import { currentTheme, initTheme, switchTheme } from '@/composables/theme.js';
+import Button from '@/components/button.vue';
+import { ArrowRightIcon } from '@/components/icons.js';
+
+onMounted(() => {
+  initTheme();
+});
+</script>
+
+<template>
+  <Button
+    :icon="ArrowRightIcon"
+    text="go"
+    @click="switchTheme"
+  />
+</template>
+
+<style></style>
