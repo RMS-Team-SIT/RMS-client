7d0baf87c0adb4067d7d0b4112941086224d5cea
siraom15 <siriwatjaiyungyuen@gmail.com>
2023-11-16 18:49:22 +0700

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ module.exports = {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
+      colors:{
+        'dark-blue': "#002B5B",
+        'light-red': "#EA5455",
+        'cream' : "#E4DCCF",
+        'light-cream' : "#F9F5EB"
+      }
     },
   },
 };
