532a6589fa68a61647d26894b75dc8848c8ddc6c
siraom15 <siriwatjaiyungyuen@gmail.com>
2023-12-03 16:32:05 +0700

    upd

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import { createPinia } from 'pinia';
 import Notifications from '@kyvg/vue3-notification';
 import Vue3EasyDataTable from 'vue3-easy-data-table';
 import 'vue3-easy-data-table/dist/style.css';
+import VueSweetalert2 from 'vue-sweetalert2';
+// import 'sweetalert2/dist/sweetalert2.min.css';
+import '@sweetalert2/themes/material-ui/meterial-ui.scss';
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -15,10 +18,11 @@ const app = createApp(App);
 app.component('EasyDataTable', Vue3EasyDataTable);
 
 app.use(Notifications);
+app.use(VueSweetalert2);
 app.use(pinia);
 app.use(router);
 
 app.mount('#app');
 
-console.log(import.meta.env.MODE)
-console.log(import.meta.env.VITE_MODE)
+console.log(import.meta.env.MODE);
+console.log(import.meta.env.VITE_MODE);
