6a3170728024d443749a9ceb781989763cf5a2ae
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-03-03 09:07:25 +0700

    aaa

--- src/pages/admin/dashboard.vue
+++ src/pages/admin/dashboard.vue
@@ -31,29 +31,50 @@ const isLoading = ref(true);
 const userStats = reactive({
   totalPendingUsers: {
     title: 'จำนวนผู้ใช้งานที่ขอการอนุมัติ',
-    val: 20,
+    val: 0,
     desc: 'จำนวนผู้ใช้งานที่รอการอนุมัติ',
     icon: ClockIcon,
     route: 'admin-manage-residence',
   },
   totalUsers: {
     title: 'จำนวนผู้ใช้งานทั้งหมด',
-    val: 10,
+    val: 0,
     desc: 'จำนวนผู้ใช้งานทั้งหมดที่ผ่านการยืนยันตัวตนแล้ว',
     icon: UserIcon,
     route: 'admin-manage-user',
   },
   totalAdmins: {
     title: 'จำนวนผู้ดูแลระบบทั้งหมด',
-    val: 10,
+    val: 0,
     desc: 'จำนวนผู้ดูแลระบบทั้งหมด',
     icon: UserIcon,
     route: 'admin-manage-user',
   },
 });
-const mapStatsToArray = computed(() => {
+const residenceStats = reactive({
+  totalApprovedResidences: {
+    title: 'จำนวนหอพักที่ผ่านการอนุมัติ',
+    val: 0,
+    desc: 'จำนวนหอพักที่ผ่านการอนุมัติทั้งหมด',
+    icon: HomeIcon,
+    route: 'admin-manage-residence',
+  },
+  totalPendingResidences: {
+    title: 'จำนวนหอพักที่รอการอนุมัติ',
+    val: 0,
+    desc: 'จำนวนหอพักที่รอการอนุมัติ',
+    icon: ClockIcon,
+    route: 'admin-manage-user',
+  },
+});
+
+const mapUserStatsToArray = computed(() => {
   return Object.values(userStats);
 });
+const mapResidenceStatsToArray = computed(() => {
+  return Object.values(residenceStats);
+});
+
 const fetchStats = async () => {
   try {
     const response = await UserServices.overAllStats();
@@ -69,6 +90,10 @@ const fetchStats = async () => {
     const response = await ResidenceServices.overAllStats();
     if (response.status === 200) {
       const residenceStats = await response.json();
+      const { totalApprovedResidences, totalPendingResidences } =
+        residenceStats;
+      residenceStats.totalApprovedResidences.val = totalApprovedResidences;
+      residenceStats.totalPendingResidences.val = totalPendingResidences;
     }
   } catch (error) {}
 };
@@ -111,11 +136,49 @@ const approveUser = async (userId) => {
   } catch (error) {}
 };
 
-const toApproveResidence = ref([]);
+const pendingResidence = ref([]);
+const fetchPendingResidence = async () => {
+  try {
+    const response = await ResidenceServices.pendingApprove();
+    if (response.status === 200) {
+      pendingResidence.value = await response.json();
+      console.log('pendingResidence', pendingResidence.value);
+    } else {
+      notify({
+        group: 'tr',
+        title: 'เกิดข้อผิดพลาด',
+        title: 'ไม่สามารถดึงข้อมูลหอพักที่รอการอนุมัติได้',
+        type: 'error',
+      });
+    }
+  } catch (error) {}
+};
+const approveResidence = async (id) => {
+  try {
+    const response = await ResidenceServices.approveResidence(id);
+    if (response.status === 200) {
+      notify({
+        group: 'tr',
+        title: 'อนุมัติผู้ใช้งานสำเร็จ',
+        text: 'อนุมัติผู้ใช้งานสำเร็จแล้ว',
+        type: 'success',
+      });
+      await fetchAll();
+    } else {
+      notify({
+        group: 'tr',
+        title: 'เกิดข้อผิดพลาด',
+        text: 'ไม่สามารถอนุมัติผู้ใช้งานได้',
+        type: 'error',
+      });
+    }
+  } catch (error) {}
+};
 
 const fetchAll = async () => {
   await fetchStats();
   await fetchPendingKYCUser();
+  await fetchPendingResidence();
   isLoading.value = false;
 };
 onMounted(async () => {
@@ -134,7 +197,7 @@ onMounted(async () => {
           { name: 'จัดการระบบ', pathName: 'manage' },
         ]"
       />
-      <Stats :stats="mapStatsToArray" class="mt-5" />
+      <Stats :stats="mapUserStatsToArray" class="mt-5" />
 
       <div
         class="relative bg-white p-10 mt-5 space-y-4 rounded border border-gray-200"
@@ -147,7 +210,9 @@ onMounted(async () => {
           <ClockIcon class="h-8 w-8 text-primary" />
         </h1>
         <!-- <p class="text-xs">ผู้ใช้งานที่รอการอนุมัติทั้งหมด</p> -->
-        <p class="text-sm" v-if="!pendingKYCUser.length">ไม่มีผู้ใช้งานที่รอการอนุมัติในขณะนี้</p>
+        <p class="text-sm" v-if="!pendingKYCUser.length">
+          ไม่มีผู้ใช้งานที่รอการอนุมัติในขณะนี้
+        </p>
         <div
           v-for="(user, index) in pendingKYCUser"
           :key="index"
@@ -204,6 +269,7 @@ onMounted(async () => {
         </div>
       </div>
 
+      <Stats :stats="mapResidenceStatsToArray" class="mt-5" />
       <div
         class="relative bg-white p-10 mt-5 space-y-4 rounded border border-gray-200"
       >
@@ -213,8 +279,63 @@ onMounted(async () => {
           หอพักที่รอการอนุมัติ
           <HomeIcon class="h-8 w-8 text-primary" />
           <ClockIcon class="h-8 w-8 text-primary" />
+          
         </h1>
-        <p class="text-sm">ไม่มีหอพักที่รอการอนุมัติในขณะนี้</p>
+        <p class="text-sm" v-if="!pendingResidence">ไม่มีหอพักที่รอการอนุมัติในขณะนี้</p>
+        <div
+          v-for="(user, index) in pendingResidence"
+          :key="index"
+          class="flex items-center gap-2 justify-between border-2 p-2 rounded-lg border-gray-200 hover:border-primary transition-all cursor-pointer"
+        >
+          <div class="flex items-center gap-2">
+            <img
+              :src="BlankprofileImg"
+              alt="user profile"
+              class="h-10 w-10 rounded-full"
+            />
+            <div>
+              <h1 class="text-sm font-semibold">
+                {{ user.firstname }} {{ user.lastname }}
+              </h1>
+              <p class="text-xs">{{ user.email }}</p>
+            </div>
+          </div>
+
+          <div class="flex items-center gap-2">
+            <button
+              class="btn btn-primary btn-sm"
+              :onclick="`kyc_modal_${index}.showModal()`"
+            >
+              ดูข้อมูลเพิ่มเติม
+            </button>
+          </div>
+
+          <!-- Modal -->
+
+          <dialog :id="`kyc_modal_${index}`" class="modal">
+            <div class="modal-box space-y-2">
+              <h3 class="font-bold text-lg">รายละเอียดผู้ใช้งาน</h3>
+
+              <p>ชื่อผู้ใช้งาน : {{ user.firstname }} {{ user.lastname }}</p>
+              <p>อีเมล : {{ user.email }} (ยืนยันแล้ว)</p>
+              <p>เบอร์โทร : {{ user.phone }}</p>
+              <p>หมายเลขบัตรประชาชน : {{ user.idcardNumber }}</p>
+
+              <div class="modal-action flex">
+                <form method="dialog">
+                  <!-- if there is a button in form, it will close the modal -->
+                  <button class="btn btn-sm">ปิด</button>
+                </form>
+                <button
+                  class="btn btn-success btn-sm"
+                  @click="approveUser(user._id)"
+                >
+                  อนุมัติบัญชีผู้ใช้
+                </button>
+              </div>
+            </div>
+          </dialog>
+        </div>
       </div>
 
       <!-- Quick link Section -->
