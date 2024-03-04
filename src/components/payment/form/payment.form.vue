b6835585003ac4d96766ceb724e41212ae17148a
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-03-04 19:16:55 +0700

    fxxxxx ui

--- src/components/payment/form/payment.form.vue
+++ src/components/payment/form/payment.form.vue
@@ -96,99 +96,97 @@ watch(childData, () => {
 </script>
 
 <template>
-  <div class="flex gap-4 shadow-md rounded">
-    <div class="bg-white p-10 space-y-4">
-      <h1 class="text-xl font-semibold text-dark-blue-200">
-        ตั้งค่าช่องทางการชำระเงิน
-      </h1>
-      <p class="text-xs">โปรดใส่ข้อมูลช่องทางการชำระเงิน</p>
+  <div class="relative bg-white p-10 space-y-4 shadow-md rounded">
+    <h1 class="text-xl font-semibold text-dark-blue-200">
+      ตั้งค่าช่องทางการชำระเงิน
+    </h1>
+    <p class="text-xs">โปรดใส่ข้อมูลช่องทางการชำระเงิน</p>
 
-      <div class="flex flex-col lg:flex-row gap-5" v-if="!viewOnly">
-        <div>
-          <label class="label">
-            <span class="text-base label-text"
-              >ธนาคาร <span class="text-red-500">*</span>
-            </span>
-          </label>
-          <select
-            class="select select-bordered w-full max-w-xs select-sm bg-white input-sm rounded-sm"
-            v-model="tempPayment.bankId"
-          >
-            <option value="">เลือกธนาคาร</option>
-            <option v-for="bank in banks" :key="bank._id" :value="bank._id">
-              {{ bank.thai_name }}
-            </option>
-          </select>
-        </div>
-
-        <div>
-          <label class="label">
-            <span class="text-base label-text"
-              >ชื่อบัญชี <span class="text-red-500">*</span>
-            </span>
-          </label>
-          <input
-            type="text"
-            placeholder="ชื่อบัญชี"
-            class="input input-bordered bg-white input-sm rounded-sm"
-            v-model="tempPayment.account_name"
-          />
-        </div>
-
-        <div>
-          <label class="label">
-            <span class="text-base label-text"
-              >หมายเลขบัญชี
-              <span class="text-red-500">*</span>
-            </span>
-          </label>
-          <input
-            type="text"
-            placeholder="หมายเลขบัญชี"
-            class="input input-bordered bg-white input-sm rounded-sm"
-            v-model="tempPayment.account_number"
-          />
-        </div>
-
-        <div class="flex items-end">
-          <button
-            class="btn btn-primary btn-sm"
-            @click="addPayment"
-            :disabled="props.viewOnly"
-          >
-            เพิ่ม
-          </button>
-        </div>
-      </div>
-      <div class="flex flex-col gap-4">
-        <div
-          v-for="(payment, index) in childData.payments"
-          :key="index"
-          class="grid grid-cols-5 gap-4 items-center justify-between bg-gray-100 p-4 rounded-md"
+    <div class="flex flex-col lg:flex-row gap-5" v-if="!viewOnly">
+      <div>
+        <label class="label">
+          <span class="text-base label-text"
+            >ธนาคาร <span class="text-red-500">*</span>
+          </span>
+        </label>
+        <select
+          class="select select-bordered w-full max-w-xs select-sm bg-white input-sm rounded-sm"
+          v-model="tempPayment.bankId"
         >
-          <p><BankIcon :bank="findBank(payment.bankId)?.bank" size="xxxl" /></p>
-          <p>{{ findBank(payment.bankId)?.thai_name }}</p>
-          <p>{{ payment.account_name }}</p>
-          <p>{{ payment.account_number }}</p>
-          <button
-            v-if="!props.viewOnly"
-            @click="removePayment(index)"
-            class="btn btn-sm btn-secondary w-min"
-          >
-            ลบ
-          </button>
-        </div>
+          <option value="">เลือกธนาคาร</option>
+          <option v-for="bank in banks" :key="bank._id" :value="bank._id">
+            {{ bank.thai_name }}
+          </option>
+        </select>
       </div>
 
-      <h1 class="text-xl font-semibold text-dark-blue-200">ข้อความเพิ่มเติม</h1>
-      <p class="text-xs">ข้อความเพิ่มเติมจะแสดงพร้อมกับช่องทางการชำระเงิน</p>
-      <textarea
-        placeholder="ข้อความเพิ่มเติม"
-        class="w-full textarea textarea-bordered rounded-sm bg-white"
-        v-model="childData.paymentNotes"
-        :disabled="viewOnly"
-      ></textarea>
+      <div>
+        <label class="label">
+          <span class="text-base label-text"
+            >ชื่อบัญชี <span class="text-red-500">*</span>
+          </span>
+        </label>
+        <input
+          type="text"
+          placeholder="ชื่อบัญชี"
+          class="input input-bordered bg-white input-sm rounded-sm"
+          v-model="tempPayment.account_name"
+        />
+      </div>
+
+      <div>
+        <label class="label">
+          <span class="text-base label-text"
+            >หมายเลขบัญชี
+            <span class="text-red-500">*</span>
+          </span>
+        </label>
+        <input
+          type="text"
+          placeholder="หมายเลขบัญชี"
+          class="input input-bordered bg-white input-sm rounded-sm"
+          v-model="tempPayment.account_number"
+        />
+      </div>
+
+      <div class="flex items-end">
+        <button
+          class="btn btn-primary btn-sm"
+          @click="addPayment"
+          :disabled="props.viewOnly"
+        >
+          เพิ่ม
+        </button>
+      </div>
     </div>
+    <div class="flex flex-col gap-4">
+      <div
+        v-for="(payment, index) in childData.payments"
+        :key="index"
+        class="grid grid-cols-5 gap-4 items-center justify-between bg-gray-100 p-4 rounded-md"
+      >
+        <p><BankIcon :bank="findBank(payment.bankId)?.bank" size="xxxl" /></p>
+        <p>{{ findBank(payment.bankId)?.thai_name }}</p>
+        <p>{{ payment.account_name }}</p>
+        <p>{{ payment.account_number }}</p>
+        <button
+          v-if="!props.viewOnly"
+          @click="removePayment(index)"
+          class="btn btn-sm btn-secondary w-min"
+        >
+          ลบ
+        </button>
+      </div>
+    </div>
+
+    <h1 class="text-xl font-semibold text-dark-blue-200">ข้อความเพิ่มเติม</h1>
+    <p class="text-xs">ข้อความเพิ่มเติมจะแสดงพร้อมกับช่องทางการชำระเงิน</p>
+    <textarea
+      placeholder="ข้อความเพิ่มเติม"
+      class="w-full textarea textarea-bordered rounded-sm bg-white"
+      v-model="childData.paymentNotes"
+      :disabled="viewOnly"
+    ></textarea>
   </div>
 </template>
 
--- src/components/residence/form/residence.room-type.form.vue
+++ src/components/residence/form/residence.room-type.form.vue
@@ -80,107 +80,105 @@ watch(childData, () => {
 </script>
 
 <template>
-  <div class="flex gap-4 shadow-md rounded w-full">
-    <div class="bg-white p-10 space-y-4">
-      <h1 class="text-xl font-semibold text-dark-blue-200">ประเภทห้องพัก</h1>
-      <p class="text-xs">สร้างประเภทห้องพักในหอพักของคุณ</p>
+  <div class="relative bg-white p-10 space-y-4 shadow-md rounded">
+    <h1 class="text-xl font-semibold text-dark-blue-200">ประเภทห้องพัก</h1>
+    <p class="text-xs">สร้างประเภทห้องพักในหอพักของคุณ</p>
 
-      <div class="grid grid-cols-3 gap-5" v-if="!viewOnly">
-        <div>
-          <label class="label">
-            <span class="text-base label-text"
-              >ประเภท <span class="text-red-500">*</span>
-            </span>
-          </label>
-          <input
-            type="text"
-            placeholder="ชื่อประเภท"
-            class="input input-bordered bg-white input-sm rounded-sm"
-            v-model="temp.name"
-          />
-          <p class="text-xs text-gray-500">เช่น ห้องสตูดิโอ, 1 ห้องนอน</p>
-        </div>
-
-        <div>
-          <label class="label">
-            <span class="text-base label-text"
-              >ขนาดห้อง (ตร.ม.) <span class="text-red-500">*</span>
-            </span>
-          </label>
-          <input
-            type="number"
-            min="0"
-            placeholder="ขนาดห้อง (ตร.ม.)"
-            class="input input-bordered bg-white input-sm rounded-sm"
-            v-model="temp.size"
-          />
-          <p class="text-xs text-gray-500">เช่น 10 ตร.ม. (กรอกเฉพาะตัวเลข)</p>
-        </div>
-
-        <div>
-          <label class="label">
-            <span class="text-base label-text"
-              >ค่าเช่าบาท/เดือน <span class="text-red-500">*</span>
-            </span>
-          </label>
-          <input
-            type="number"
-            min="0"
-            placeholder="ค่าเช่าบาท/เดือน"
-            class="input input-bordered bg-white input-sm rounded-sm"
-            v-model="temp.price"
-          />
-        </div>
-
-        <div class="col-span-3">
-          <label class="label">
-            <span class="text-base label-text"
-              >รายละเอียด <span class="text-red-500">*</span>
-            </span>
-          </label>
-          <textarea
-            rows="10"
-            placeholder="รายละเอียด"
-            class="input input-bordered bg-white input-sm rounded-sm w-full"
-            v-model="temp.description"
-          ></textarea>
-        </div>
-      
-        <div class="flex items-center">
-          <button
-            class="btn btn-primary btn-sm"
-            @click="add"
-            :disabled="props.viewOnly"
-          >
-            เพิ่ม
-          </button>
-        </div>
+    <div class="grid grid-cols-3 gap-5" v-if="!viewOnly">
+      <div>
+        <label class="label">
+          <span class="text-base label-text"
+            >ประเภท <span class="text-red-500">*</span>
+          </span>
+        </label>
+        <input
+          type="text"
+          placeholder="ชื่อประเภท"
+          class="input input-bordered bg-white input-sm rounded-sm"
+          v-model="temp.name"
+        />
+        <p class="text-xs text-gray-500">เช่น ห้องสตูดิโอ, 1 ห้องนอน</p>
       </div>
 
-      <div class="flex flex-col gap-4">
-        <div
-          v-for="(payment, index) in childData.roomTypes"
-          :key="index"
-          class="grid grid-cols-5 gap-4 items-center justify-between bg-gray-100 p-4 rounded-md"
+      <div>
+        <label class="label">
+          <span class="text-base label-text"
+            >ขนาดห้อง (ตร.ม.) <span class="text-red-500">*</span>
+          </span>
+        </label>
+        <input
+          type="number"
+          min="0"
+          placeholder="ขนาดห้อง (ตร.ม.)"
+          class="input input-bordered bg-white input-sm rounded-sm"
+          v-model="temp.size"
+        />
+        <p class="text-xs text-gray-500">เช่น 10 ตร.ม. (กรอกเฉพาะตัวเลข)</p>
+      </div>
+
+      <div>
+        <label class="label">
+          <span class="text-base label-text"
+            >ค่าเช่าบาท/เดือน <span class="text-red-500">*</span>
+          </span>
+        </label>
+        <input
+          type="number"
+          min="0"
+          placeholder="ค่าเช่าบาท/เดือน"
+          class="input input-bordered bg-white input-sm rounded-sm"
+          v-model="temp.price"
+        />
+      </div>
+
+      <div class="col-span-3">
+        <label class="label">
+          <span class="text-base label-text"
+            >รายละเอียด <span class="text-red-500">*</span>
+          </span>
+        </label>
+        <textarea
+          rows="10"
+          placeholder="รายละเอียด"
+          class="input input-bordered bg-white input-sm rounded-sm w-full"
+          v-model="temp.description"
+        ></textarea>
+      </div>
+
+      <div class="flex items-center">
+        <button
+          class="btn btn-primary btn-sm"
+          @click="add"
+          :disabled="props.viewOnly"
         >
-          <div class="col-span-2">
-            <p class="text-base font-semibold">{{ payment.name }}</p>
-            <p class="text-xs text-gray-500">{{ payment.description }}</p>
-          </div>
-          <div class="col-span-1">
-            <p class="text-base">{{ payment.size }} ตร.ม.</p>
-          </div>
-          <div class="col-span-1">
-            <p class="text-base">{{ payment.price }} บาท/เดือน</p>
-          </div>
-          <button
-            v-if="!props.viewOnly"
-            @click="remove(index)"
-            class="btn btn-sm btn-secondary w-min"
-          >
-            ลบ
-          </button>
+          เพิ่ม
+        </button>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-4">
+      <div
+        v-for="(payment, index) in childData.roomTypes"
+        :key="index"
+        class="grid grid-cols-5 gap-4 items-center justify-between bg-gray-100 p-4 rounded-md"
+      >
+        <div class="col-span-2">
+          <p class="text-base font-semibold">{{ payment.name }}</p>
+          <p class="text-xs text-gray-500">{{ payment.description }}</p>
         </div>
+        <div class="col-span-1">
+          <p class="text-base">{{ payment.size }} ตร.ม.</p>
+        </div>
+        <div class="col-span-1">
+          <p class="text-base">{{ payment.price }} บาท/เดือน</p>
+        </div>
+        <button
+          v-if="!props.viewOnly"
+          @click="remove(index)"
+          class="btn btn-sm btn-secondary w-min"
+        >
+          ลบ
+        </button>
       </div>
     </div>
   </div>
