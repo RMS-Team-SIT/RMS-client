ef0f4eab96496ad0491fe5ff59e6840c387e16e5
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-03-27 16:06:07 +0700

    ff

--- src/components/common/footer.vue
+++ src/components/common/footer.vue
@@ -15,19 +15,19 @@ const thisYear = new Date().getFullYear();
     </aside>
     <nav>
       <header class="footer-title">Services</header>
-      <a class="link link-hover">Manage</a>
-      <a class="link link-hover">Dashboard</a>
+      <div class="link link-hover">Manage</div>
+      <div class="link link-hover">Dashboard</div>
     </nav>
     <nav>
       <header class="footer-title">Company</header>
-      <a class="link link-hover">About us</a>
-      <a class="link link-hover">Contact</a>
+      <div class="link link-hover">About us</div>
+      <div class="link link-hover">Contact</div>
     </nav>
     <nav>
       <header class="footer-title">Legal</header>
-      <a class="link link-hover">Terms of use</a>
-      <a class="link link-hover">Privacy policy</a>
-      <a class="link link-hover">Cookie policy</a>
+      <div class="link link-hover">Terms of use</div>
+      <div class="link link-hover">Privacy policy</div>
+      <div class="link link-hover">Cookie policy</div>
     </nav>
   </footer>
   <p class="p-2 w-full text-center text-sm">&copf; RMS team</p>
